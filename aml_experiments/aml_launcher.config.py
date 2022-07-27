984dc1d64ff30341183ba1d9969e5908434c930c
Felipe Vieira Frujeri <felipe.frujeri@microsoft.com>
2022-07-27 14:30:06 -0700

    Remove GUIDs.

--- aml_experiments/aml_launcher.config.py
+++ aml_experiments/aml_launcher.config.py
@@ -11,21 +11,14 @@ from azureml.data import OutputFileDatasetConfig
 
 from azureml_connector import compute_manager, environment_manager
 
-# interactive_auth = InteractiveLoginAuthentication(
-#     tenant_id="TENANT_ID"
-# )
-# ws = Workspace(
-#     subscription_id="SUBSCRIPTION_ID",
-#     resource_group="RESOURCE_GROUP",
-#     workspace_name="WOKSPACE_NAME",
-#     auth=interactive_auth,
-# )
-
-interactive_auth = InteractiveLoginAuthentication(tenant_id="72f988bf-86f1-41af-91ab-2d7cd011db47")
-ws = Workspace(subscription_id="d347a80f-7b72-4114-96cc-b2faa4ed5344",
-               resource_group="fevieira-rg",
-               workspace_name="fevieira-aml-ws",
-               auth=interactive_auth
+interactive_auth = InteractiveLoginAuthentication(
+    tenant_id="TENANT_ID"
+)
+ws = Workspace(
+    subscription_id="SUBSCRIPTION_ID",
+    resource_group="RESOURCE_GROUP",
+    workspace_name="WOKSPACE_NAME",
+    auth=interactive_auth,
 )
 
 root_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
