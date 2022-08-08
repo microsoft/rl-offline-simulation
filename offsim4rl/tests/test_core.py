8c1768eb8a2ee05a413830609189e36e45db6069
sebastko <sebastko@microsoft.com>
2022-08-08 16:30:21 -0700

    revealed randomness env + tests

---
+++ offsim4rl/tests/test_core.py
@@ -0,0 +1,41 @@
+import gym
+import numpy as np
+import torch
+import pytest
+from torch.distributions import Categorical
+from offsim4rl.core import RevealedRandomnessEnvWrapper
+
+def test_revealed_randomness_env_numpy():
+    env = gym.make('CartPole-v0')
+    env = RevealedRandomnessEnvWrapper(env)
+    assert hasattr(env, 'step_dist')
+    obs = env.reset()
+    assert type(obs) == np.ndarray
+    action, next_obs, reward, done, info = env.step_dist(np.array([0.5, 0.5]))
+    assert type(action) == np.int64
+    assert type(next_obs) == np.ndarray
+    assert type(reward) == float
+    assert type(done) == bool
+    assert type(info) == dict
+
+def test_revealed_randomness_env_torch():
+    env = gym.make('CartPole-v0')
+    env = RevealedRandomnessEnvWrapper(env)
+    assert hasattr(env, 'step_dist')
+    obs = env.reset()
+    assert type(obs) == np.ndarray
+    action, next_obs, reward, done, info = env.step_dist(Categorical(torch.tensor([0.5, 0.5])))
+    assert type(action) == torch.Tensor
+    assert type(next_obs) == np.ndarray
+    assert type(reward) == float
+    assert type(done) == bool
+    assert type(info) == dict
+
+def test_revealed_randomness_env_other():
+    env = gym.make('CartPole-v0')
+    env = RevealedRandomnessEnvWrapper(env)
+    assert hasattr(env, 'step_dist')
+    obs = env.reset()
+    assert type(obs) == np.ndarray
+    with pytest.raises(ValueError):
+        env.step_dist(0)
