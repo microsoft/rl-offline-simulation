0ed092641d1ba988b3fd73ed98522cf3d4ebd011
Felipe Vieira Frujeri <felipe.frujeri@microsoft.com>
2022-09-10 17:05:09 -0700

    Update ppo_agent_rollout to use PPOAgent.

--- examples/cartpole/ppo_agent_rollout.py
+++ examples/cartpole/ppo_agent_rollout.py
@@ -1,21 +1,12 @@
 # Collect data for cartpole useing ppo agent
 import argparse
-from collections import defaultdict
-import os
 
 import gym
-import gzip
-import h5py
-import matplotlib.pyplot as plt
-import numpy as np
-import random
-
-from spinup.algos.pytorch.ppo import core
-from spinup.algos.pytorch.ppo.ppo import ppo
 from spinup.utils.logx import EpochLogger
-from spinup.utils.mpi_pytorch import setup_pytorch_for_mpi, sync_params, mpi_avg_grads
-from spinup.utils.mpi_tools import mpi_fork, mpi_avg, proc_id, mpi_statistics_scalar, num_procs
+from spinup.utils.mpi_tools import mpi_fork, proc_id
+from spinup.utils.run_utils import setup_logger_kwargs
 
+from offsim4rl.agents.ppo import PPOAgent
 
 def main():
     parser = argparse.ArgumentParser()
@@ -26,46 +17,38 @@ def main():
     parser.add_argument('--seed', '-s', type=int, default=0)
     parser.add_argument('--cpu', type=int, default=4)
     parser.add_argument('--steps', type=int, default=4000)
-    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--num_iter', type=int, default=50000)
     parser.add_argument('--exp_name', type=str, default='ppo')
     parser.add_argument('--output_dir', type=str, default='./outputs')
 
     args = parser.parse_args()
 
-    os.makedirs(args.output_dir, exist_ok=True)
+    logger_kwargs = setup_logger_kwargs(args.exp_name, 0, data_dir=args.output_dir)
+    logger = EpochLogger(**logger_kwargs)
 
-    mpi_fork(args.cpu)  # run parallel code with mpi
-
-    from spinup.utils.run_utils import setup_logger_kwargs
-    logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed, data_dir=args.output_dir)
-
-    ppo(
-        lambda : gym.make(args.env),
-        actor_critic=core.MLPActorCritic,
+    env = gym.make(args.env)
+    agent = PPOAgent(
+        env.observation_space,
+        env.action_space,
         ac_kwargs=dict(hidden_sizes=[args.hid] * args.l),
+        logger=logger,
         gamma=args.gamma,
         seed=args.seed,
         steps_per_epoch=args.steps,
-        epochs=args.epochs,
-        logger_kwargs=logger_kwargs
     )
 
-    # random.seed(args.seed)
-    # np.random.seed(args.seed)
-    # rng = np.random.default_rng(seed=args.seed)
+    mpi_fork(args.cpu)  # run parallel code with mpi
+    num_interactions = args.num_iter
+    obs, _ = env.reset(seed=args.seed)
+    a, logp = agent.begin_episode(obs)
+    for t in range(num_interactions):
+        obs, r, terminated, truncated, _ = env.step(a)
+        a, logp = agent.step(r, obs, terminated, truncated)
 
-    # env = gym.make('CartPole-v1', new_step_api=True)
-    # agent = DiscreteRandom(env.action_space)
-    # os.makedirs(TEST_OUTPUT_DIR, exist_ok=True)
-    # path = os.path.join(TEST_OUTPUT_DIR, 'cartpole.hdf5')
-
-    # dataset = record_dataset_in_memory(
-    #     env,
-    #     agent,
-    #     num_samples=1000,
-    #     seed=98052,
-    #     new_step_api=new_step_api)
-    # dataset.save_hdf5(path)
+        if terminated or truncated:
+            agent.end_episode(r)
+            obs, _ = env.reset(seed=args.seed)
+            a, logp = agent.begin_episode(obs)
 
 
 if __name__ == "__main__":
--- offsim4rl/agents/ppo.py
+++ offsim4rl/agents/ppo.py
@@ -41,12 +41,14 @@ class PPOAgent(Agent):
         self.observation_space = observation_space
         self.seed = seed
 
+        self.seed += 10000 * proc_id()
+        torch.manual_seed(self.seed)
+        np.random.seed(self.seed)
+
         setup_pytorch_for_mpi()
         self.logger = logger
         self.logger.save_config(locals())
 
-        torch.manual_seed(self.seed)
-        np.random.seed(self.seed)
         self.ac = actor_critic(observation_space, action_space, **ac_kwargs)
 
         self.steps_per_epoch = steps_per_epoch
@@ -246,50 +248,3 @@ class PPOAgent(Agent):
     def _compute_loss_v(self, transitions):
         obs, ret = transitions['obs'], transitions['ret']
         return ((self.ac.v(obs) - ret) ** 2).mean()
-
-
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--env', type=str, default='HalfCheetah-v2')
-    parser.add_argument('--hid', type=int, default=64)
-    parser.add_argument('--l', type=int, default=2)
-    parser.add_argument('--gamma', type=float, default=0.99)
-    parser.add_argument('--seed', '-s', type=int, default=0)
-    parser.add_argument('--cpu', type=int, default=4)
-    parser.add_argument('--steps', type=int, default=4000)
-    parser.add_argument('--num_iter', type=int, default=50000)
-    parser.add_argument('--exp_name', type=str, default='ppo')
-    parser.add_argument('--output_dir', type=str, default='./outputs')
-
-    args = parser.parse_args()
-
-    from spinup.utils.run_utils import setup_logger_kwargs
-    logger_kwargs = setup_logger_kwargs(args.exp_name, 0, data_dir=args.output_dir)
-    logger = EpochLogger(**logger_kwargs)
-    seed = args.seed
-    seed += 10000 * proc_id()
-    env = gym.make(args.env)
-    agent = PPOAgent(
-        env.observation_space,
-        env.action_space,
-        actor_critic=core.MLPActorCritic,
-        ac_kwargs=dict(hidden_sizes=[args.hid] * args.l),
-        logger=logger,
-        gamma=args.gamma,
-        seed=seed,
-        steps_per_epoch=args.steps,
-    )
-
-    mpi_fork(args.cpu)  # run parallel code with mpi
-    num_interactions = args.num_iter
-    obs, _ = env.reset(seed=seed)
-    a, logp = agent.begin_episode(obs)
-    for t in range(num_interactions):
-        obs, r, terminated, truncated, _ = env.step(a)
-        a, logp = agent.step(r, obs, terminated, truncated)
-
-        if terminated or truncated:
-            agent.end_episode(r)
-            obs, _ = env.reset(seed=seed)
-            a, logp = agent.begin_episode(obs)
