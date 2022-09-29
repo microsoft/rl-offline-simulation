8c2d64e2a44ac254fe77b9dedd4944f89d7a9826
Felipe Vieira Frujeri <felipe.frujeri@microsoft.com>
2022-09-29 15:10:02 -0700

    Make ppo compatible with discrete obs_space.

--- offsim4rl/agents/ppo.py
+++ offsim4rl/agents/ppo.py
@@ -70,7 +70,11 @@ class PPOAgentRevealed(Agent):
         self.target_kl = target_kl
         self.save_freq = save_freq
 
-        self.obs_dim = 1
+        if isinstance(observation_space, gym.spaces.Box):
+            self.obs_dim = observation_space.shape[0]
+        elif isinstance(observation_space, gym.spaces.Discrete):
+            self.obs_dim = 1
+
         self.act_dim = action_space.shape
 
         # Sync params across processes
